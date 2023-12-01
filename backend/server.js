380349c9462725885fdb5403891d3eabc4a18ee7
Isaac Orraiz <isaacorraiz@gmail.com>
2023-12-01 01:35:38 -0500

    Server js typo fixed on tickets.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const express = require("express");
-require("colors");
-require("dotenv").config();
+const colors = require("colors");
+const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
-// Connect to database
+//Connect to DB
 connectDB();
 
 const app = express();
@@ -14,25 +14,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
+//Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 
-// Serve Frontend
+//Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  // Set build folder as static
+  //Set build folder as static folder
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  // FIX: below code fixes app crashing on refresh in deployment
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
 } else {
-  app.get("/", (_, res) => {
+  app.get("/", (req, res) => {
     res.status(200).json({ message: "Welcome to the Support Desk API" });
   });
 }
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
